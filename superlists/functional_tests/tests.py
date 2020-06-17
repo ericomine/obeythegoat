567f44b3f943319db33c3c6e47b51fab32373368
Eric Omine <eric.omine@gmail.com>
2020-06-17 20:21:10 -0300

    Use LiveServerTestCase

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_list_and_retrieve(self):
         # User opens to-do app
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # User notices page title mentions todo
         self.assertIn('Todo', self.browser.title)
@@ -62,6 +62,3 @@ class NewVisitorTest(unittest.TestCase):
         # By visiting the URL, todo list is shown.
 
         # User exits site.
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
