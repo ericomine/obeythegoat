ac15fa02ac9d70cf279d3bde016e677c59d9682c
Eric Omine <eric.omine@gmail.com>
2020-06-17 20:33:35 -0300

    Removing calls to time.sleep()

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -15,6 +18,21 @@ class NewVisitorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                # This is the same as check_for_row_in_list_table (removed)
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if (time.time() - start_time > MAX_WAIT):
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_list_and_retrieve(self):
         # User opens to-do app
@@ -51,8 +69,8 @@ class NewVisitorTest(LiveServerTestCase):
         # Page updates, showing both items
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('finish the test')
 
